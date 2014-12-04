a85e9c98e020f90c8429609b1189defe7c31bad2
ramille4 <ramille4@ncsu.edu>
2014-12-04 15:02:25 -0500

    progress bars in js

--- js/script.js
+++ js/script.js
@@ -28,4 +28,28 @@
     $('#newheader').addClass("ui-widget-header ui-corner-all");
     $('#newheader').prepend("<span class='ui-icon ui-icon-minusthick portlet-toggle'></span>");
   });
+     $( "#pb1" ).progressbar({
+      value: 37
+    });
+    $( "#pb2" ).progressbar({
+      value: 43
+    });
+    $( "#pb3" ).progressbar({
+      value: 81
+    });
+    $( "#pb4" ).progressbar({
+      value: 23
+    });
+    $( "#pb5" ).progressbar({
+      value: 95
+    });
+    $( "#pb6" ).progressbar({
+      value: 1
+    });
+    $( "#pb7" ).progressbar({
+      value: 100
+    });
+    $( "#pb8" ).progressbar({
+      value: 100
+    });
 });
